52a47c33864dddd060110880a55649a30c80d081
Amos SOSSOUKPE <fenouamossossoukpe@gmail.com>
2023-06-12 22:28:48 +0200

    projet backend avec express et des bibliotheques jwt, bodyparser, mariadb, sequelize

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const sequelize = require("./config/database");
+
+
+//Initialisation SERVEUR DE BASE DE DONNES
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+//Initialisation SERVEUR D'APPLICATION
+const app = express()
+
+/*l'erreur cors*/
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
